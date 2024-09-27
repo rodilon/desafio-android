832fd7c08e18442f5bc652f344e0150ef9a8d3a0
rodilon <renanodilon@gmail.com>
2024-09-26 22:30:09 -0300

    refactor: uses retrofit 2 on api interface

--- app/src/main/java/br/com/renan/desafioandroid/model/api/Api.kt
+++ app/src/main/java/br/com/renan/desafioandroid/model/api/Api.kt
@@ -2,18 +2,22 @@ package br.com.renan.desafioandroid.model.api
 
 import br.com.renan.desafioandroid.model.data.PullRequest
 import br.com.renan.desafioandroid.model.data.RepositoryItemsList
-import io.reactivex.Flowable
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface Api {
     @GET("search/repositories?")
-    fun getRepositories(@Query("q") language: String,
-                        @Query("sort") stars: String,
-                        @Query("page") page: Int): Flowable<RepositoryItemsList>
+    suspend fun getRepositories(
+        @Query("q") language: String,
+        @Query("sort") stars: String,
+        @Query("page") page: Int,
+    ): Response<RepositoryItemsList>
 
     @GET("repos/{login}/{name}/pulls?state=all")
-    fun getPullRequests(@Path(value = "login") login: String,
-                        @Path(value = "name") name: String): Flowable<List<PullRequest>>
+    suspend fun getPullRequests(
+        @Path(value = "login") login: String,
+        @Path(value = "name") name: String,
+    ): Response<List<PullRequest>>
 }
